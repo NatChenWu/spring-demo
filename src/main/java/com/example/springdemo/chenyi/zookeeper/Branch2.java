c2d2c39b439637026da22b386260f53b212a598c
chenyi <chenyi@kanzhun.com>
2020-06-06 11:07:27 +0800

    brand2

---
+++ src/main/java/com/example/springdemo/chenyi/zookeeper/Branch2.java
@@ -0,0 +1,9 @@
+package com.example.springdemo.chenyi.zookeeper;
+
+/**
+ * @author: chenyi
+ * @date: 2020-06-06:11:06
+ * @desciption:
+ */
+public class Branch2 {
+}
---
+++ src/main/java/com/example/springdemo/chenyi/zookeeper/Branch2Test.java
@@ -0,0 +1,9 @@
+package com.example.springdemo.chenyi.zookeeper;
+
+/**
+ * @author: chenyi
+ * @date: 2020-06-06:11:07
+ * @desciption:
+ */
+public class Branch2Test {
+}
